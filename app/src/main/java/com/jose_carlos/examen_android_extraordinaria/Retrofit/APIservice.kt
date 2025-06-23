7bbbac467eaf28b5983c274f9aeb39abcfd9ba08
Unicaja1984 <luquecastrojosecarlos@gmail.com>
2025-06-23 15:32:46 +0200

    Trabajando en el Recycler

---
+++ app/src/main/java/com/jose_carlos/examen_android_extraordinaria/Retrofit/APIservice.kt
@@ -0,0 +1,16 @@
+package com.jose_carlos.examen_android_extraordinaria.Retrofit
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface APIservice {
+
+    @GET("recipes")
+    suspend fun getAllRecipes ():Response<RecetaResponse>
+    // Tener en cuenta que se ha de usar el Response de Retrofit, no el de OKhttp3
+
+    // Endpoint para buscar por el id del superheroe:
+    @GET("recipes/{recipes-id}")
+    suspend fun searchById (@Path("recipes-id") recipeId: String): Response<DatosReceta>
+}
--- app/src/main/java/com/jose_carlos/examen_android_extraordinaria/Retrofit/RetrofitClient.kt
+++ app/src/main/java/com/jose_carlos/examen_android_extraordinaria/Retrofit/RetrofitClient.kt
@@ -1,5 +1,18 @@
 package com.jose_carlos.examen_android_extraordinaria.Retrofit
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitClient {
+
+    // Añado la URL base:
+    private const val BASE_URL = "https://dummyjson.com/"
+
+    val instance: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 
 }
